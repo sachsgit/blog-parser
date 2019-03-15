93842c29119f92cd653aa2f2b678d3f303691d9a
SS007979 <Shaule.Sachs@penfed.org>
2019-03-15 11:56:37 -0400

    Adding my code

---
+++ src/main/java/com/blogger/words_from_desire/Main.java
@@ -0,0 +1,35 @@
+package com.blogger.words_from_desire;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import com.sangupta.blogparser.blogger.BloggerParser;
+import com.sangupta.blogparser.domain.Blog;
+import com.sangupta.blogparser.domain.BlogPost;
+
+public class Main {
+    private static final String FS = File.separator;
+    private static final String BLOGFILE = System.getenv("FW_WG") + FS + "Parshot" + FS + "blog-03-14-2019.xml";
+
+    public static void main(String[] args) throws IOException {
+        FileReader reader = null;
+        try {
+            reader = new FileReader(new File(BLOGFILE));
+            BloggerParser parser = new BloggerParser();
+            Blog blog = parser.parse(reader);
+            List<BlogPost> posts = blog.getPosts();
+            for (int i = 0; i < posts.size(); i++) {
+                System.out.println("Title: " + posts.get(i).getTitle() + ", Published On: " + posts.get(i).getPublishedOn().toString());
+            }
+            System.out.println("Posts Count: " + posts.size());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            reader.close();
+        }
+    }
+
+}
