a5c402e803a53b6769903f3bdad373a222fef3f5
SS007979 <Shaule.Sachs@penfed.org>
2019-04-02 16:27:46 -0400

    Sync with L61102

--- src/main/java/com/blogger/words_from_desire/Main.java
+++ src/main/java/com/blogger/words_from_desire/Main.java
@@ -4,7 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.sangupta.blogparser.blogger.BloggerParser;
 import com.sangupta.blogparser.domain.Blog;
@@ -12,10 +16,16 @@ import com.sangupta.blogparser.domain.BlogPost;
 
 public class Main {
     private static final String FS = File.separator;
-    private static final String BLOGFILE = System.getProperty("user.dir") + FS + "src" + FS 
-        + "main" + FS + "resources" + FS + "blog-03-14-2019.xml";
+    private static final String LOGPATH = Paths.get(System.getProperty("user.dir") + FS + "Properties" + FS + "log4j2.xml").toUri().toString();
+    private static final String LOGPARAM = "log4j.configurationFile";
+    private static final String BLOGFILE = System.getProperty("user.dir") + FS + "src" + FS + "main" + FS + "resources" + FS + "blog-03-14-2019.xml";
+    
+    static {
+        System.setProperty(LOGPARAM, LOGPATH);
+    }
 
     public static void main(String[] args) throws IOException {
+        Logger logger = LogManager.getLogger(Main.class);
         FileReader reader = null;
         try {
             reader = new FileReader(new File(BLOGFILE));
@@ -23,16 +33,17 @@ public class Main {
             Blog blog = parser.parse(reader);
             List<BlogPost> posts = blog.getPosts();
             for (int i = 0; i < posts.size(); i++) {
-                System.out.println("Title[" + i + "]: " + posts.get(i).getTitle() + ", Published On: " 
+                logger.info("Title[" + i + "]: " + posts.get(i).getTitle().trim() + ", Published On: " 
                     + posts.get(i).getPublishedOn().toString());
             }
-            System.out.println("Posts Count: " + posts.size());
+            logger.info("Posts Count: " + posts.size());
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            logger.error("Main.FileNotFoundException: " + e.getMessage(), e);
         } finally {
             if (reader != null)
                 reader.close();
         }
+        logger.debug("Done.");
     }
 
 }
